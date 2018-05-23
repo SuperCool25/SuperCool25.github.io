cac05866fb0626d025720128f8ef7bc7e10ff52f
SuperCool25 <38873365+SuperCool25@users.noreply.github.com>
2018-05-22 21:54:15 -0600

    Update loop.js

--- loop.js
+++ loop.js
@@ -76,8 +76,6 @@ initButtons();
 function loop() {
     ctx.fillStyle = "green";
     ctx.fillRect(0, 0, 500, 700);
-
-   // on = document.getElementById("mobile").checked;
     
     if (car.x < 74) {
         speed -= 1.2;
